703489d1226aeb58f37766d132b6ee0961225b36
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-09-04 22:42:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "3498045"))
+API_HASH = os.environ.get("API_HASH", "229441a8b355de58ba025be657c82ea3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6685547719:AAF7OlfoKOChF4TBEFjPwtp-eeGUakTr9f4")
 
 CREDIT = os.environ.get("CREDIT", "〱ＵＧ▕")
 # MongoDB Configuration
@@ -59,3 +59,4 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
